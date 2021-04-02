e4d1156dcb251634974a53383324bce380760694
HasanQZ <zahrafamily215@gmail.com>
2021-04-02 18:14:04 -0500

    modified:   aurinstall.c 	deleted:    json.h 	new file:   simple-json

--- c/aurinstall.c
+++ c/aurinstall.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
 #include "requests.h"
-#include "json.h"
+#include "simple-json/simple_json.h"
 
-int
-main() {
+int main() {
     char* x = get("https://aur.archlinux.org/rpc/?v=5&type=info&arg[]=ungoogled-chromium");
-    // printf("%s\n", x);
-
-    char* e = "{"
-    "\"name\":\"John\","
-    "\"age\":18,"
-    "\"friends\": [\"Bob\",\"Jake\",\"Daryl\"]"
-    "}";
-
-    parse_package_json(x);
+    printf("%s\n", x);
 }
