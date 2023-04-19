771d98b21e74c2d8613fe6ced4c8622b1e1ab3ef
Hasan Zahra <hasan@hasanzahra.xyz>
2023-04-19 05:10:21 -0500

    fixed debug code of using assert for allocations

--- alloc.c
+++ alloc.c
@@ -23,26 +23,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
+
+void checkmem(void *ptr)
+{
+        if (ptr == NULL) {
+                fprintf(stderr, "error: failed to allocate memory.\n");
+                exit(1);
+        }
+}
 
 void *safe_malloc(size_t s)
 {
         void *mem = malloc(s);
-        assert(mem != NULL);
+        checkmem(mem);
         return mem;
 }
 
 void *safe_calloc(int n, size_t s)
 {
         void *mem = calloc(n, s);
-        assert(mem != NULL);
+        checkmem(mem);
         return mem;
 }
 
 void *safe_realloc(void *ptr, size_t s)
 {
         void *mem = realloc(ptr, s);
-        assert(mem != NULL);
+        checkmem(mem);
         return mem;
 }
 
